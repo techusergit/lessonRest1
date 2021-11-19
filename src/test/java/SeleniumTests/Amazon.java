35d75b211a3c9007f319819c2d269be8e40fc66e
techusergit <technicaluser078@gmail.com>
2021-11-19 15:20:59 +0200

    update test

--- src/test/java/SeleniumTests/Amazon.java
+++ src/test/java/SeleniumTests/Amazon.java
@@ -1,13 +1,16 @@
 package SeleniumTests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,9 +20,16 @@ public class Amazon {
 
     @BeforeTest
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "C:\\MyPrograms\\WebDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/home/user/projects/common/study_plan/workspace/lessonRest1/src/test/resources/chromedriver");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
+
+    @AfterTest()
+    public void tearDown() {
+        driver.quit();
+    }
+
     @Test
     public void amazonTest(){
         driver.get("https://www.amazon.com");
@@ -27,16 +37,38 @@ public class Amazon {
         element.sendKeys("Man shoes");
         element.submit();
         Assert.assertEquals(driver.getCurrentUrl(),"https://www.amazon.com/s?k=Man+shoes&ref=nb_sb_noss");
-        List <WebElement> shoes = driver.findElements(By.xpath("//span[@class =\"a-price-whole\"]"));
+
+        List<WebElement> list = driver.findElements(By.xpath("//div[@class='a-section a-spacing-medium']"));
+        List<Shoe> shoes = new ArrayList<>();
+
+        //driver.getPageSource();
+
+        list.forEach(el -> {
+            try {
+                Shoe shoe = new Shoe(el.findElement(By.xpath(".//span[contains(@class,'a-text-normal')]")).getText(),
+                        el.findElement(By.xpath(".//span[contains(@class,'a-price')]/span")).getAttribute("innerText"));
+                shoes.add(shoe);
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println(shoes);
+
+
+
+//        list.get(0).findElement(By.xpath(".//span[contains(@class,'a-text-normal')]")).getText();
+//        list.get(0).findElement(By.xpath(".//span[contains(@class,'a-price')]/span")).getAttribute("innerText");
+
+
+/*        List<WebElement> shoes = driver.findElements(By.xpath("//span[@class =\"a-price-whole\"]"));
         Map <String, String> map = new HashMap<String, String>();
 //           for (WebElement elem : shoes){
 //               map.put(elem.getText(), )
 //            }
-
          int i = Integer.parseInt(shoes.get(0).getText());
          System.out.println(i);
-
-
+*/
 
 //        for (int i= 0; i < shoes.size(); i++){
 //            System.out.println(shoes.get(i).getText());
@@ -45,4 +77,15 @@ public class Amazon {
 ////span[@class ="a-price-whole"]/../../../../../..//span[@class ="a-size-base-plus a-color-base a-text-normal"]
 
     }
+
+    class Shoe {
+        private String name;
+        private String price;
+
+        public Shoe(String name, String price) {
+            this.name = name;
+            this.price = price;
+        }
+    }
+
 }
